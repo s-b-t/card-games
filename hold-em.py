991b701d9bcc19451510c2c4f6774fa18c5c037a
Steve-Tobias <sblaket@gmail.com>
2024-09-13 09:08:24 -0600

    fixed comments

--- hold-em.py
+++ hold-em.py
@@ -4,13 +4,13 @@ import secrets
 ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 suits = [' ♥︎', ' ♠︎', ' ♦︎', ' ♣︎']
 
-# Create the deck of cards as a list of (rank, suit) tuples
+# Create the deck of cards as a list of (rank + suit) tuples
 deck = [(rank + suit) for rank in ranks for suit in suits]
 
 # Shuffle the deck using the CSRNG (cryptographically secure random number generator)
 secrets.SystemRandom().shuffle(deck)
 
-# Function to deal cards (i.e. 2 cards)
+# Function to deal a hand (i.e. 2 cards)
 def dealHand():
     return [deck.pop(0) for _ in range(2)]
 
@@ -19,21 +19,21 @@ def burnCard():
     return deck.pop(0)
 
 def dealFlop():
-    # Burn the top card and deal the next 3 cards in the list (Flop cards)
+    # Burn the top card and deal the next 3 cards in the deck (Flop cards)
     burnCard()
     return [deck.pop(0) for _ in range(3)]
 
 def dealTurn():
-    # Burn the top card and deal the next card in the list (Turn card)
+    # Burn the top card and deal the next card in the deck (Turn card)
     burnCard()
     return deck.pop(0)
 
 def dealRiver():
-    # Burn the top card and deal the next card in the list (River card)
+    # Burn the top card and deal the next card in the deck (River card)
     burnCard()
     return deck.pop(0)
 
-# Example: Deal a 2-card hand, the Flop (3 cards), the Turn (1 card), and the River (1 card)
+# Example: Deal a hand (2 cards), the Flop (3 cards), the Turn (1 card), and the River (1 card)
 hand = dealHand()
 flop = dealFlop()
 turn = dealTurn()
